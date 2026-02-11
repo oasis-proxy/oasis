2110d2028ccbcbeca10f779ee98f2ae0a294cee7
oasis <167432768+oasis-proxy@users.noreply.github.com>
2026-02-11 11:58:00 +0800

    build: Remove generated build output and update Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./src/manifest.json";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), crx({ manifest })],
+  build: {
+    rollupOptions: {
+      input: {
+        monitor: resolve(__dirname, "src/monitor/index.html"),
+      },
+    },
+  },
 });
